8eef202637c53259ccbd0a91a8172bae245db589
Amelie Dubois <ameduboi@c2r12p4.42lehavre.fr>
2025-04-30 22:57:42 +0200

    export with no parsing

--- src/built-in/ft_export.c
+++ src/built-in/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: ameduboi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 22:21:44 by ameduboi          #+#    #+#             */
-/*   Updated: 2025/04/30 02:36:07 by ameduboi         ###   ########.fr       */
+/*   Updated: 2025/04/30 22:55:42 by ameduboi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,20 @@ void	ft_export(t_minishell *minishell, char **export)
 	int		j;
 	char	**new_env;
 
+	i = ft_tablen(minishell->env) + ft_tablen(export);
+	new_env = malloc(i + 1 * sizeof(char *));
+	if (!new_env)
+		return (NULL);
 	i = 0;
-	j = 0;
 	while (minishell->env[i])
-		minishell->env[i++];
-	while (export[j])
-		j++;
-	new_env = malloc(i + j + 1 * sizeof(char *));
-	while ()
 	{
-		
+		new_env[i] = ft_strdup(minishell->env[i]);
+		i++;
 	}
+	j = 0;
+	while (export[i])
+		new_env[i++] = ft_strdup(export[j++]);
+	new_env[i] = '\0';
+	ft_free_tab(minishell->env);
+	minishell->env = ft_tabdup(new_env);
 }
