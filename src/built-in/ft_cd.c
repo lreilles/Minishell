623fbb1d71beaec630836785cad55ddaae0b4f4f
Amelie Dubois <ameduboi@c2r9p5.42lehavre.fr>
2025-05-13 02:51:51 +0200

    update cd

--- src/built-in/ft_cd.c
+++ src/built-in/ft_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsellier <lsellier@student.42lehavre.fr    +#+  +:+       +#+        */
+/*   By: ameduboi <ameduboi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 22:32:35 by ameduboi          #+#    #+#             */
-/*   Updated: 2025/05/13 02:32:54 by lsellier         ###   ########.fr       */
+/*   Updated: 2025/05/13 02:43:53 by ameduboi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,17 @@ int	ft_cd(t_minishell *minishell, char **cmd)
 {
 	int		verif;
 	char	*path;
+	int		tmp;
 
+	tmp = 0;
 	if (ft_tablen(cmd) > 1)
 		return (ft_dprintf(2, "minishell: cd: too many arguments\n"), 1);
 	verif = chdir(cmd[0]);
 	if (verif < 0)
 		return (1);
-	path = get_env_value(minishell, minishell->env, "PWD", NULL);
+	path = get_env_value(minishell, minishell->env, "PWD", &tmp);
 	if (path)
-	{
-		free(path);
 		edit_env_value(minishell, "PWD", cmd[0]);
-	}
 	free(path);
 	return (0);
 }
