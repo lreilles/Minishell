729f622720846e31b451b993453c6606cc1bda6a
Amelie Dubois <ameduboi@c1r7p8.42lehavre.fr>
2025-05-14 17:04:47 +0200

    built-in update

--- src/built-in/ft_cd.c
+++ src/built-in/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: ameduboi <ameduboi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 22:32:35 by ameduboi          #+#    #+#             */
-/*   Updated: 2025/05/13 03:30:49 by ameduboi         ###   ########.fr       */
+/*   Updated: 2025/05/14 17:03:26 by ameduboi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ int	ft_cd(t_minishell *minishell, char **cmd)
 		return (ft_dprintf(2, "minishell: cd: too many arguments\n"), 1);
 	verif = chdir(cmd[0]);
 	if (verif < 0)
-		return (ft_dprintf(2, "cd: no such file or directory: cd\n"),1);
+		return (ft_dprintf(2, "cd: %s: No such file or directory\n",
+				cmd[0]), 1);
 	path = get_env_value(minishell, minishell->env, "PWD", &tmp);
 	if (path)
 	{
--- src/built-in/ft_unset.c
+++ src/built-in/ft_unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_unset.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ameduboi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ameduboi <ameduboi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 18:47:17 by ameduboi          #+#    #+#             */
-/*   Updated: 2025/05/06 19:08:06 by ameduboi         ###   ########.fr       */
+/*   Updated: 2025/05/13 04:15:41 by ameduboi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_unset(t_minishell *minishell, char **unset)
 	int	i;
 
 	i = 0;
-
+	
 		return (-1);
 	return (0);
 }
