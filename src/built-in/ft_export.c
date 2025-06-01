b2ae9d2cbd32c7ab38f921bf0f562a9c3e9c6322
Pika337 <amelie.dubois337@gmail.com>
2025-06-02 00:43:31 +0200

    export sans aucun test mais peut-etre fini

--- src/built-in/ft_export.c
+++ src/built-in/ft_export.c
@@ -14,43 +14,81 @@
 
 // edit_env_value(t_minishell *shell, char *value, char *new_one)
 
-void	ft_export(t_minishell *minishell, char **export)
+char	*before_equal(char *value)
 {
-	char	*shlvl;
-	int		temp;
+	int		i;
+	char	*result;
 
-	temp = 0;
-	shlvl = get_env_value(*shell, (*shell)->env, "SHLVL", &temp);
-	if (shlvl)
-	{
-		temp = ft_atoi(shlvl) + 1;
-		free(shlvl);
-		shlvl = ft_itoa(temp);
-		edit_env_value(*shell, "SHLVL", shlvl);
-	}
-	free(shlvl);
+	i = 0;
+	while (value[i] != '=')
+		i++;
+	result = malloc(i + 1);
+	result = ft_strncpy(result, value, i);
+	return(result);
 }
 
-void	ft_export_with_value(t_minishell *minishell, char **export)
+char	*after_equal(char *value)
+{
+	int		i;
+	int		j;
+	char	*result;
+
+	i = 0;
+	while (value[i] != '=')
+		i++;
+	i++;
+	j = ft_strlen(value) - i;
+	result = malloc(j + 1);
+	j = 0;
+	while (value[i] != '\0')
+		result[j++] = value[i++];
+	result[j] = '\0';
+	return (result);
+}
+
+void	ft_export(t_minishell *shell, char **export)
+{
+	char	*result;
+	char	*first_value;
+	int		i;
+	char	*end_value;
+
+	i = 0;
+	while (export[i])
+	{
+		first_value = before_equal(export[i]);
+		result = get_env_value(*shell, (*shell)->env, first_value, 0);
+		if (result)
+		{
+			free(result);
+			end_value = after_equal(export[i]);
+			edit_env_value(*shell, first_value, end_value);
+		}
+		else
+			ft_export_with_value(shell, export[i]);
+		i++;
+	}
+	free(result);
+}
+
+void	ft_export_with_value(t_minishell *shell, char *export)
 {
 	int		i;
 	int		j;
 	char	**new_env;
 
-	i = ft_tablen(minishell->env) + ft_tablen(export);
+	i = ft_tablen(shell->env) + ft_tablen(export);
 	new_env = malloc(i + 1 * sizeof(char *));
 	if (!new_env)
 		return (NULL);
-	i = 0;
-	while (minishell->env[i])
-	{
-		new_env[i] = ft_strdup(minishell->env[i]);
-		i++;
-	}
+	i = ft_tablen(shell->env) + 1;
 	j = 0;
-	while (export[i])
-		new_env[i++] = ft_strdup(export[j++]);
-	new_env[i] = '\0';
-	ft_free_tab(minishell->env);
-	minishell->env = ft_tabdup(new_env);
+	while (export[j])
+	{
+		new_env[i][j] = export[j];
+		j++;
+	}
+	new_env[i][j] = '\0';
+	ft_free_tab(shell->env);
+	shell->env = ft_tabdup(new_env);
 }
